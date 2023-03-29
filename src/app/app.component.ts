3afe8255eaf13f537e385f39c2923b2c94a6062a
RubenBaader <rubenbaader@gmail.com>
2023-03-30 00:14:30 +0200

    now handles async product load on keystroke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { KeyValue } from '@angular/common';
-import { Component, OnInit, Inject, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { fromEvent, of, from, Observable, filter } from 'rxjs';
+import { of, Observable, exhaustMap } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs';
 import { GetProductsService } from './get-products/get-products.service';
 import { Product, SearchProductResponse } from './get-products/products';
@@ -18,47 +18,57 @@ export class AppComponent implements OnInit {
   title = 'projectSearch';
 
   constructor( private productGetter: GetProductsService) { }
-  
 
   // The hashmap data structure allows for very fast searches because each key is hashed to a value between 0 and n-1 (n being the map's length)
   // The hash value maps to an address in memory and we do not have to look through the entire structure to find our key, unlike looking through a list
-  productList : SearchProductResponse | undefined;
+  productList : Product[] | undefined;
+
   searchResult : Product[] = [];
   searchControl = new FormControl();
+  searchTerm = "";
+  search$ = this.searchControl.valueChanges.pipe(
+    tap(x => this.searchTerm = x),
+    debounceTime(250),
+    distinctUntilChanged()
+  );
 
-  // productList is populated on init because I did not have time to figure out how to load it only on first search.
-    // The simplest implementation I can think of would be to set a productListLoaded = true flag in the valueChanges pipe.
-    // Then if(!productListLoaded){ getProductList() } in the pipe should create the correct behaviour.
+  // inner method
+  getProductsFromService = () => {
+    return this.productGetter.getProductList(this.productGetter.productUrl)
+  }
+  // outer method
+  getProductList = () => {
+    if(!this.productList) {
+    return this.getProductsFromService().pipe(
+      map(data => { return this.productList = data.content } )
+      );
+    } else
+    return of(this.productList);
+  }
+
+  filterProducts(productArr : Product[] , searchTerm : string) {
+    return productArr.filter(item => {
+      return item.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+    })
+  }
+
+  // This is the search pipe.
+    // search$ handles the search input and starts the pipeline, storing the search term for later
+    // exhaustMap with getProductList loads the products, ignoring any new search inputs from the input stream
+    // switchmap cancels any current filtering if I change the search term - after the product list has loaded
+  // The pipe "pipes" data through itself, ensuring that the fast .filter method in filterProducts does not run before it has anything to filter.
   ngOnInit(): void {
-    this.getProductList()
-
-    this.searchControl.valueChanges.pipe(
-      debounceTime(250),
-      tap(x =>  this.searchResult = []),
-      switchMap(changedValue => this.getProducts(changedValue))
-   ).subscribe(data => {
+    this.search$.pipe(
+      exhaustMap(this.getProductList),
+      tap(x => {this.productList = x as Product[]; this.searchResult = [] }),
+      switchMap(data => this.filterProducts(data as Product[], this.searchTerm)),
+    ).subscribe(data => {
       this.searchResult.push(data);
-    });
-  }
-
-  getProductList() {
-    this.productGetter.getProductList().subscribe(data => {
-      this.productList = data;
     })
   }
 
-  // working search method - not free text
-  getProducts(searchTerm : string) {
-    let toFilter : Product[] = this.productList!.content!;
-    
-    return toFilter.filter(item => {
-      return item.title.toLocaleLowerCase().includes(searchTerm);
-    })
-  }
-  
   // I attempted free text search but could not get it working in time. My thoughts on getting it working:
     // I would split search term and product.title into arrays.
     // Then I would create a double loop to compare each index and return the product for matching indexes
     // Ideally, I would then sort the resulting array by number of matches, to boost relevancy
-
 }
--- src/app/get-products/get-products.service.ts
+++ src/app/get-products/get-products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
@@ -7,58 +7,30 @@ import { catchError, retry, take } from 'rxjs/operators';
 
 import { Product, SearchProductResponse } from './products';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class GetProductsService {
   productUrl = 'assets/products.json'
 
-  // C:\Users\Bruger\Documents\GitHub\projectsearch\src\assets\products.json
-
   constructor(private http : HttpClient) { }
 
-  testInjectable() {
-    console.log("Hello from GetProductService!");
+
+  getProductList(input : string) {
+    return this.http.get<SearchProductResponse>(input)
   }
 
-  getJson() {
-    const a : Observable<Product> = this.http.get<Product>(this.productUrl)
-      .pipe(
-        take(5),
-        retry(3),
-        catchError(this.handleError)
-      )
-    // console.log(a.subscribe(), "ASYNC #######################################")
-    /* return this.http.get<Product>(this.productUrl)
-      .pipe(
-        retry(3),
-        catchError(this.handleError)
-      ) */
-    // console.log(this.productUrl)
-    return a;
-  }
-
-  getProductList() {
-    return this.http.get<SearchProductResponse>(this.productUrl)
-  }
-
-  getObservableJson(): Observable<HttpResponse<Product>> {
-    return this.http.get<Product>(
-      this.productUrl, { observe: 'response' } );
-  }
 
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
+      console.error('An error occurred:', error.error);         //client-side error
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
     }
     // Return an observable with a user-facing error message.
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
-
 }
